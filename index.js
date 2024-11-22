8fc5eb0b60bd8cc84fd034d6c707650f4a019cc2
CodingSwordfish <wagobryan@gmail.com>
2024-11-22 16:56:10 +0300

    Add styling and a click event for each square and add the logic to render the 9 squares

--- index.js
+++ index.js
@@ -4,20 +4,56 @@ let gameboard = ["","","","","","","","",""];
 const render = ()=>{
     let boardHTML = "";
     gameboard.forEach((square,index)=>{
-        boardHTML += `<div class="square" id="square-${index}"></div>`
+        boardHTML += `<div class="square" id="square-${index}">${square}</div>`
     })
     document.querySelector("#gameboard").innerHTML = boardHTML;
+const squares = document.querySelectorAll(".square");
+squares.forEach((square)=>{
+    square.addEventListener("click",handleClick)
+})
+
 }
 return {
-    render,
+  render,
 }
 
 })()
 
+const createPlayer = (name,mark)=>{
+    return{
+        name,
+        mark
+    }
+}
 
+const Game = (()=>{
+    let players = [];
+    let currentPlayerIndex ;
+    let gameOver;
+
+
+    const start = ()=>{
+        players = [
+            createPlayer(document.querySelector("#player1").value,"X"),
+            createPlayer(document.querySelector("#player2").value,"O"),
+        ]
+// console.log(players[0].name)
+        currentPlayerIndex = 0; 
+        gameOver = false;
+        if(players[0].name && players[1].name){
+            Gameboard.render();
+        }
+       
+    }
+
+    return {
+        start
+    }
+
+})()
 
 
 const startButton = document.querySelector("#startButton");
 startButton.addEventListener("click",(e)=>{
-//Game.start
+Game.start();
 })
